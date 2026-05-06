642574ac8f51930d2ee71749e8a5b60bdb619265
ibmgitpankaj <pankaj.pareek2@ibm.com>
2026-05-06 22:13:33 +0530

    Initial Terraform commit (intentionally non-compliant)

---
+++ demo.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "demo-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "demostorage12345"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
